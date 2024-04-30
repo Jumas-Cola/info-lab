ae5a4b7ee56b1c7b8942e5b88c93a6650051fbbf
Jumas <kbbyfl91@gmail.com>
2024-04-30 15:14:15 +0300

    Добавлен шаблон Scholar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+// Import our custom CSS
+import '../css/app.scss';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
+import.meta.glob([
+  '../images/**',
+  '../fonts/**',
+]);
---
+++ resources/views/components/main-layout.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+    <title>Scholar - Online School HTML5 Template</title>
+
+    @vite(['resources/css/app.scss'])
+
+    <!-- Additional CSS Files -->
+    <!-- <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css" /> -->
+    <!--
+
+TemplateMo 586 Scholar
+
+https://templatemo.com/tm-586-scholar
+
+-->
+</head>
+
+<body>
+
+    <!-- ***** Preloader Start ***** -->
+    <!-- <div id="js-preloader" class="js-preloader"> -->
+    <!--     <div class="preloader-inner"> -->
+    <!--         <span class="dot"></span> -->
+    <!--         <div class="dots"> -->
+    <!--             <span></span> -->
+    <!--             <span></span> -->
+    <!--             <span></span> -->
+    <!--         </div> -->
+    <!--     </div> -->
+    <!-- </div> -->
+    <!-- ***** Preloader End ***** -->
+
+    <x-nav.header/>
+
+    {{ $slot }}
+
+    <x-nav.footer/>
+
+    <!-- Scripts -->
+    <!-- Bootstrap core JavaScript -->
+    <!-- <script src="vendor/bootstrap/js/bootstrap.min.js"></script> -->
+    <!-- <script src="assets/js/isotope.min.js"></script> -->
+    <!-- <script src="assets/js/owl-carousel.js"></script> -->
+    <!-- <script src="assets/js/counter.js"></script> -->
+    <!-- <script src="assets/js/custom.js"></script> -->
+    @vite(['resources/js/app.js'])
+
+</body>
+
+</html>
