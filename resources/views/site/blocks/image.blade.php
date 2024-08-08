0a8a9fd4e309ac9b046d2d252e5ae9f912037895
Jumas <kbbyfl91@gmail.com>
2024-08-08 23:46:32 +0300

    Исправлены картинки блога

--- resources/views/site/blocks/image.blade.php
+++ resources/views/site/blocks/image.blade.php
@@ -2,7 +2,7 @@
     $imgArr = $block->imageAsArray('highlight', 'desktop');
 @endphp
 
-<div class="pt-6 m-auto d-flex align-items-center">
+<div class="pt-6 m-auto d-flex align-items-center justify-content-center">
     <figure>
       <img src="{{ $imgArr['src'] }}" alt="{{ $imgArr['alt'] }}" />
       <figcaption>{{ $imgArr['caption'] }}</figcaption>
