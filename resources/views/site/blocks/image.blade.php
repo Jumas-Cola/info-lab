1e35f42a09fa689400ff6b137030cd9a72c3342c
Jumas <kbbyfl91@gmail.com>
2024-08-08 23:07:03 +0300

    Добавлены подписи к картинкам блога

--- resources/views/site/blocks/image.blade.php
+++ resources/views/site/blocks/image.blade.php
@@ -1,3 +1,6 @@
 <div class="pt-6 m-auto d-flex align-items-center">
-    <img src="{{ $block->image('highlight', 'desktop') }}" />
+    <figure>
+      <img src="{{ $block->image('highlight', 'desktop') }}" alt="{{ $block->input('alt') }}" />
+      <figcaption>{{ $block->input('caption') }}</figcaption>
+    </figure>
 </div>
--- resources/views/twill/blocks/image.blade.php
+++ resources/views/twill/blocks/image.blade.php
@@ -1,8 +1,18 @@
 @twillBlockTitle('Image')
 @twillBlockIcon('text')
 @twillBlockGroup('app')
- 
+
 <x-twill::medias
     name="highlight"
     label="Highlight"
+/>
+
+<x-twill::input
+    name="alt"
+    label="Alt"
+/>
+
+<x-twill::input
+    name="caption"
+    label="Caption"
 />
