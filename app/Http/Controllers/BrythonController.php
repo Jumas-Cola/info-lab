3d1ff74f20d71f31f3def9b18041f71bdd0ac111
Jumas <kbbyfl91@gmail.com>
2024-10-13 17:41:39 +0300

    Обновлен BrythonController

--- app/Http/Controllers/BrythonController.php
+++ app/Http/Controllers/BrythonController.php
@@ -10,9 +10,29 @@ class BrythonController extends Controller
         {
             public string $id = 'brython_interpreter';
 
-            public string $default_input = 'Hello, World!';
+            public string $default_input = '1000';
 
-            public string $default_code = 'import sys\n\n\nprint(sys.stdin.read())\n';
+            public string $default_code = 'import sys
+
+
+def sieve_of_eratosthenes(max_number):
+    primes = [i for i in range(max_number + 1)]
+
+    primes[1] = 0
+
+    for i in range(2, max_number + 1):
+        if primes[i] != 0:
+            for j in range(i * 2, max_number + 1, i):
+                primes[j] = 0
+
+    primes = [i for i in primes if i != 0]
+
+    return primes
+
+max_number = int(sys.stdin.read())
+primes = sieve_of_eratosthenes(max_number)
+print(primes)
+            ';
 
             public mixed $children = null;
         };
