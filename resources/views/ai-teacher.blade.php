bac807a732605d4bf7f2b61e8aa9aca7163feb48
Jumas <kbbyfl91@gmail.com>
2024-10-25 01:09:46 +0300

    Исправлен AI ассистент

--- resources/views/ai-teacher.blade.php
+++ resources/views/ai-teacher.blade.php
@@ -41,7 +41,7 @@
                             </div>
 
                             <div class="form-outline d-flex mt-3">
-                                <textarea x-model="message" @keyup.enter="send" class="form-control bg-body-tertiary border-1" id="textAreaExample"
+                                <textarea x-model="message" @keyup.shift.enter="send" class="form-control bg-body-tertiary border-1" id="textAreaExample"
                                     rows="4"></textarea>
                                 <div class="d-flex justify-content-center align-items-center">
                                     <button class="btn btn-primary rounded-circle ms-2" type="button" @click="send">
