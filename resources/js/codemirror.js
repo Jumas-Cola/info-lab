853f6e1826d2c5c595da17765e7d0cdc3ac788d5
Jumas <kbbyfl91@gmail.com>
2024-10-31 01:24:17 +0300

    Добавлена тема tokyo-night в редактор codemirror

--- resources/js/codemirror.js
+++ resources/js/codemirror.js
@@ -1,6 +1,8 @@
 import { EditorView, basicSetup } from 'codemirror';
 import { python } from '@codemirror/lang-python';
+import { tokyoNight } from '@uiw/codemirror-theme-tokyo-night';
 
 window.EditorView = EditorView;
 window.basicSetup = basicSetup;
+window.tokyoNight = tokyoNight;
 window.python = python;
